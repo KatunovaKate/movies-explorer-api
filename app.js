c46753377cc25ac628d7b3f4d987d3f5b3911c9d
Катунова Екатерина <rfneyjdfrfnz@yandex.ru>
2022-05-16 13:08:59 +0300

    cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { cors } = require('./middlewares/cors');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
